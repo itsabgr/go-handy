e8747e49b9b6dac8f4980ad3a2bd744789ba39fc
ABGR <tbpmtbpm@gmail.com>
2022-05-21 13:49:00 +0430

    Update pointer.go

--- pointer.go
+++ pointer.go
@@ -3,8 +3,8 @@ package handy
 import "unsafe"
 
 //Ref returns a uintptr points to the value
-func Ref[T any](value T) uintptr {
-	return uintptr(unsafe.Pointer(&value))
+func Ref[T any](value *T) uintptr {
+	return uintptr(unsafe.Pointer(value))
 }
 
 //DeRef returns the value pointer points to
