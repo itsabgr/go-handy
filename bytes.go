d0c73eae1ce4d2530a33ad59c69e3b89d69b3e2a
ABGR <tbpmtbpm@gmail.com>
2022-05-22 12:14:50 +0430

    Update bytes.go

--- bytes.go
+++ bytes.go
@@ -3,6 +3,6 @@ package handy
 //Clone makes a copy of bytes
 func Clone(src []byte) []byte {
 	clone := make([]byte, len(src))
-	Assert(copy(clone, src) == len(src), nil)
+	Assert(copy(clone, src) == len(src))
 	return clone
 }
