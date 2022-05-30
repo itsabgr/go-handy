91eab1ef2d5d5f1c049bbb99d3fd4f160a53649c
abgr <tbpmtbpm@gmail.com>
2022-05-30 14:49:07 +0430

    impl

--- closer.go
+++ closer.go
@@ -1,10 +1,7 @@
 package handy
 
-import "io"
-
-//Close closes the closable without error
-func Close(closable io.Closer) {
+func Just(closable func() error) {
 	if closable != nil {
-		_ = closable.Close()
+		_ = closable()
 	}
 }
